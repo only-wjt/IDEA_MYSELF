9512104bc8f84587d22cbf0bcbe89a4f11128190
onlywjt <Tao541880>
2021-04-23 17:02:04 +0800

    Initial commit

---
+++ onlyGitTest/src/test.java
@@ -0,0 +1,10 @@
+/**
+ * @autor onlyWjt
+ * @date 2021年04月23日 4:57 下午
+ * @desc
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("xxxxx");
+    }
+}
